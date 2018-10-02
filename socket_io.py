1c8eb5b94eba508443cd6f0f8234bce0c1bf6f1b
Kevin Matthews <kmatthews@cyberdata.net>
2018-10-02 16:15:51 -0700

    Start eventlet server with socketio server as middleware

--- socket_io.py
+++ socket_io.py
@@ -1,1 +1,12 @@
 import socketio
+import eventlet
+
+SERVER_HOSTNAME = ''
+SERVER_PORT = 8000
+
+sio = socketio.Server()
+
+if __name__ == '__main__':
+    application = socketio.Middleware(sio)
+    listener = eventlet.listen((SERVER_HOSTNAME, SERVER_PORT))
+    eventlet.wsgi.server(listener, application)
