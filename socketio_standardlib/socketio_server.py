cecb40e52ba6315d37a28164d66ea7fa4d987665
Kevin Matthews <kmatthews@cyberdata.net>
2018-10-03 15:16:49 -0700

    Unfinished work on socketio server uwing wsgiref server

--- socketio_standardlib/socketio_server.py
+++ socketio_standardlib/socketio_server.py
@@ -1,3 +1,9 @@
+# Use python's built-in WSGI http server, simple_server, and the standard library's threading.
+# This simple example gives no provision for serving html pages.
+# It simple forwards all traffic to the socketio app.
+#
+# Load the socketio client HTML and javascript in a browser manually.
+
 import socketio
 import wsgiref.simple_server
 
@@ -6,7 +12,25 @@ SERVER_PORT = 8000
 
 # SocketIO (and the underlying EngineIO) can (does?) require threading.
 # I don't know if they use it by default.
-app = socketio.Server(async_mode='threading')
+sio = socketio.Server(async_mode='threading')
+app = socketio.Middleware(sio)
+
+# SocketIO events
+@sio.on('connect')
+def connect(sid, environ):
+    print('connect:', sid)
+
+@sio.on('disconnect')
+def disconnect(sid):
+    print('disconnect:', sid)
+
+@sio.on('message')
+def message(sid, data):
+    print('message:', data)
+
+@sio.on('echo')
+def echo(sid, data):
+    pass
 
 if __name__ == '__main__':
     # python-socketio's Server object doesn't seem to provide an HTTP server.
