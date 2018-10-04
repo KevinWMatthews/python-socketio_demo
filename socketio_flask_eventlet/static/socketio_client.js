c16ca9e057dcdd350db54d6d9dbb73dac3f5ec3e
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-10-03 21:23:40 -0700

    Modernize js callbacks

--- socketio_flask_eventlet/static/socketio_client.js
+++ socketio_flask_eventlet/static/socketio_client.js
@@ -2,7 +2,7 @@
 const socketIOUrl = 'http://localhost:5000';
 const socketIO = io(socketIOUrl);
 
-var logElement = document.getElementById('socketio_log');
+let logElement = document.getElementById('socketio_log');
 
 function clearLog(logElement) {
     logElement.innerText = '';
@@ -15,25 +15,25 @@ function addToLog(logElement, text) {
 //
 // Connection events
 //
-socketIO.on('connect', function() {
+socketIO.on('connect', () => {
     addToLog(logElement, `Connected: ${socketIO.id}`);
 });
-socketIO.on('connect_error', function(error) {
+socketIO.on('connect_error', (error) => {
     addToLog(logElement, 'Connect error');
 });
-socketIO.on('connect_timeout', function(error) {        // error object
+socketIO.on('connect_timeout', (error) => {
     addToLog(logElement, `Connect timeout: ${error}`);
 });
-socketIO.on('disconnect', function(reason) {
+socketIO.on('disconnect', (reason) => {
     addToLog(logElement, `Disconnected: ${reason}`);
     if (reason === 'io server disconnect') {
         socketIO.connect();
     }
 });
-socketIO.on('reconnect', function(attemptNumber) {
+socketIO.on('reconnect', (attemptNumber) => {
     addToLog(logElement, `Reconnected: ${attemptNumber}`);
 });
-socketIO.on('reconnect_failed', function() {
+socketIO.on('reconnect_failed', () => {
     addToLog('Reconnect failed');
 });
 
@@ -41,12 +41,12 @@ socketIO.on('reconnect_failed', function() {
 // Message events
 //
 // 'message' is the default event for a SocketIO server's send() call.
-socketIO.on('message', function(data) {
+socketIO.on('message', (data) => {
     addToLog(logElement, `Received message: ${data}`);
 });
 
 // Custom event
-socketIO.on('echo', function(data) {
+socketIO.on('echo', (data) => {
     addToLog(logElement, `Received echo: ${data}`);
 });
 
@@ -54,30 +54,30 @@ socketIO.on('echo', function(data) {
 //
 // Button handlers
 //
-var btn_send = document.getElementById('btn_send');
-btn_send.addEventListener('click', function(event) {
+let btn_send = document.getElementById('btn_send');
+btn_send.addEventListener('click', (event) => {
     // Sends a 'message' event to the SocketIO server
     let data = 'Clicked the Send button.';
     addToLog(logElement, data);
     socketIO.send(data);
 })
 
-var btn_echo = document.getElementById('btn_echo');
-btn_echo.addEventListener('click', function(event) {
+let btn_echo = document.getElementById('btn_echo');
+btn_echo.addEventListener('click', (event) => {
     let data = 'Clicked the Echo button!';
     addToLog(logElement, data);
     socketIO.emit('echo', data);
 });
 
-var btn_clear = document.getElementById('btn_clear');
-btn_clear.addEventListener('click', function(event) {
+let btn_clear = document.getElementById('btn_clear');
+btn_clear.addEventListener('click', (event) => {
     clearLog(logElement);
 });
 
-var btn_client_callback = document.getElementById('btn_client_callback');
-btn_client_callback.addEventListener('click', function(event) {
+let btn_client_callback = document.getElementById('btn_client_callback');
+btn_client_callback.addEventListener('click', (event) => {
     let data = 'Clicked the Client Callback button';
-    socketIO.emit('client_callback', data, function(server_rsp1, server_rsp2) {
+    socketIO.emit('client_callback', data, (server_rsp1, server_rsp2) => {
         addToLog(logElement, 'In client callback:');
         addToLog(server_rsp1);
         addToLog(server_rsp2);
@@ -88,6 +88,6 @@ btn_client_callback.addEventListener('click', function(event) {
 //
 // Manipulate the DOM
 //
-window.onload = function() {
+window.onload = () => {
     addToLog(logElement, `Run the SocketIO server on ${socketIOUrl}`);
 }
