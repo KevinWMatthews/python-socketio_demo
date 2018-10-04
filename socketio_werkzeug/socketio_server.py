e7f4c92a39866d180641b442f276b8ad3d3eabca
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-10-03 20:09:19 -0700

    Wrap application with SocketIO

--- socketio_werkzeug/socketio_server.py
+++ socketio_werkzeug/socketio_server.py
@@ -7,6 +7,10 @@ SERVER_PORT = 5000
 
 application = Response('Wiring check')
 
+# The threading model is passed to the underlying communication protocol, EngineIO
+sio = socketio.Server(async_mode='threading')
+application = socketio.Middleware(sio, application)
+
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
     run_simple(SERVER_HOSTNAME, SERVER_PORT, application, use_debugger=True, use_reloader=True)
