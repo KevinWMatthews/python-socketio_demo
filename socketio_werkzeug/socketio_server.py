3b415b52a3c3c0cfb6989835567d02159f95c0d2
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-10-03 20:12:32 -0700

    Add unserved socketio html and js, add socketio events to server

--- socketio_werkzeug/socketio_server.py
+++ socketio_werkzeug/socketio_server.py
@@ -11,6 +11,23 @@ application = Response('Wiring check')
 sio = socketio.Server(async_mode='threading')
 application = socketio.Middleware(sio, application)
 
+# SocketIO events
+@sio.on('connect')
+def connect(sid, environ):
+    print('connect:', sid)
+
+@sio.on('disconnect')
+def disconnect(sid):
+    print('disconnect:', sid)
+
+@sio.on('message')
+def message(sid, data):
+    print('message:', data)
+
+@sio.on('echo')
+def echo(sid, data):
+    pass
+
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
     run_simple(SERVER_HOSTNAME, SERVER_PORT, application, use_debugger=True, use_reloader=True)
