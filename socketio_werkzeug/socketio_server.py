c7a9b77a5adca80bb28308af714ffb87430c1618
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-10-03 20:06:53 -0700

    Start basic werkzeug dev server

--- socketio_werkzeug/socketio_server.py
+++ socketio_werkzeug/socketio_server.py
@@ -1,2 +1,12 @@
 import socketio
+from werkzeug.wrappers import Response
 from werkzeug.serving import run_simple
+
+SERVER_HOSTNAME = ''
+SERVER_PORT = 5000
+
+application = Response('Wiring check')
+
+if __name__ == '__main__':
+    from werkzeug.serving import run_simple
+    run_simple(SERVER_HOSTNAME, SERVER_PORT, application, use_debugger=True, use_reloader=True)
